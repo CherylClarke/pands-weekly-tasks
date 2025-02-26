4fc6535857505dd62b66cd4dd4004f67f29a4425
unknown <cherylclark90@live.co.uk>
2025-02-26 20:50:57 +0000

    bank.py

---
+++ bank.py
@@ -0,0 +1,8 @@
+# prompt the user and read in two money amounts in (cents)
+# add the two amounts
+#print out the answer in a human readable format wuth a euro sign and decimal point between the euro and cent of the amount
+
+
+
+number = int( input("enter amount 1: "))
+answer = number 
