6fe696a38bfb9482c81d93f5c512d7dde87468e9
unknown <cherylclark90@live.co.uk>
2025-04-26 18:00:25 +0100

    task one explained

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,11 @@
+## weekly task 01.
 
+# this file should contain a python program that displays "Hello World" when it is run.
 
+# The Python program is the print() function, in which you type print followed by brackets that inclose the characters "Hello World!"
+#  which makes a string by adding the quotation marks each side
 
 
 print("Hello World")
+
+# References in Readme
