5065b82beb750e9a54f8dc79c4720bf53dff8202
unknown <cherylclark90@live.co.uk>
2025-04-26 18:19:06 +0100

    why

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,7 @@
 # this file should contain a python program that displays "Hello World" when it is run.
 
 # The Python program is the print() function, in which you type print followed by brackets that inclose the characters "Hello World!"
+
 #  which makes a string by adding the quotation marks each side
 
 
